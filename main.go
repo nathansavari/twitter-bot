7eec6a270d16b3bc1e9536aaf08bcda4cc86a05c
Nathan <nathan.savari.contact@gmail.com>
2022-11-06 14:56:50 +0100

    tweet when it's time

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
@@ -11,6 +12,8 @@ import (
 )
 
 func main(){
+
+	getDate()
 
 	// load .env file from given path
 	// we keep it empty it will load .env from current directory
@@ -35,27 +38,55 @@ func main(){
 
 	user, _, err := client.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{})
 	if err != nil {
-		fmt.Printf("consumerSecret : %v\n", consumerSecret)
 		fmt.Printf("err: %v\n", err)
 	}
 
 	fmt.Printf("Account: @%s (%s)\n", user.ScreenName, user.Name)
 
-	// the tweet
 
-	_, _, err = client.Statuses.Update("Test", nil)
-	if err != nil {
-		fmt.Printf("err: %v\n", err)
+	for {
+
+
+		if getDate() == "14:51:00" {
+			
+		// the tweet
+
+		date := getDate()
+
+		_, _, err = client.Statuses.Update(date, nil)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		} else {
+			fmt.Println("Twitted successfully!")
+			continue
+		}
+
+		} 
+
 	}
+	
 
-	fmt.Println("Twitted successfully!")
+	
+
+
 }
 
 func getEnv() (string, string, string, string) {
+	
 	consumerKey := os.Getenv("CONSUMER_KEY")
 	consumerSecret := os.Getenv("CONSUMER_SECRET")
 	accessToken := os.Getenv("ACCESS_TOKEN_KEY")
 	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
 
 	return consumerKey, consumerSecret, accessToken, accessTokenSecret
+}
+
+func getDate() string {
+
+    dt := time.Now()
+	now := dt.Format(("15:04:05"))
+
+	fmt.Printf("test: %v\n",now)
+
+	return now
 }
