e1e0fe3ae33e4d88d7eee473b595d5a7008575ac
Nathan <nathan.savari.contact@gmail.com>
2022-11-08 15:41:34 +0100

    consume fact api

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -27,7 +30,9 @@ func main(){
 
 	// getting env variables 
 
-	consumerKey, consumerSecret, accessToken, accessTokenSecret := getEnv()
+	consumerKey, consumerSecret, accessToken, accessTokenSecret, apiKey := getEnv()
+
+	getData(apiKey)
 
 	config := oauth1.NewConfig(consumerKey, consumerSecret)
 	token := oauth1.NewToken(accessToken, accessTokenSecret)
@@ -67,14 +72,15 @@ func main(){
 
 }
 
-func getEnv() (string, string, string, string) {
+func getEnv() (string, string, string, string, string) {
 
 	consumerKey := os.Getenv("CONSUMER_KEY")
 	consumerSecret := os.Getenv("CONSUMER_SECRET")
 	accessToken := os.Getenv("ACCESS_TOKEN_KEY")
 	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
+	apiKey := os.Getenv("API_KEY")
 
-	return consumerKey, consumerSecret, accessToken, accessTokenSecret
+	return consumerKey, consumerSecret, accessToken, accessTokenSecret, apiKey
 }
 
 func getDate() string {
@@ -83,4 +89,36 @@ func getDate() string {
 	now := dt.Format(("15:04:05"))
 
 	return now
+}
+
+func getData(apiKey string)  {
+
+	type Obj struct {
+		Fact string `json:"fact"`
+	}
+	
+	var data []Obj
+
+	
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://api.api-ninjas.com/v1/facts?limit=1", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("X-Api-Key", apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("Response: %v\n", resp)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	
+	json.Unmarshal(body, &data)
+
+	fmt.Printf("Data: %v\n", data)
 }
