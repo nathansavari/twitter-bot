27aaebc1eee8499ca669fb1a794ce02a5cf9643a
ThomasVictoria <vtom.pro@gmail.com>
2022-11-17 20:37:00 +0100

    do not load .env in production

--- main.go
+++ main.go
@@ -21,10 +21,12 @@ func main() {
 	// load .env file from given path
 	// we keep it empty it will load .env from current directory
 
-	err := godotenv.Load(".env")
+	if os.Getenv("ENV") != "PROD" {
+		err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
 	}
 
 	// getting env variables
